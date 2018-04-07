8969dbf80f339cf89cf7a2c023657eb94d5905f1
kasia-misirli <kasia.misirli@gmail.com>
2018-04-07 19:03:21 +1000

    movie search app functionality

---
+++ Kasia/wk04/5-fri/main.rb
@@ -0,0 +1,27 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+require 'pry'
+
+get '/' do
+    erb(:index)
+end
+
+get '/about' do
+    erb(:about)
+end
+
+get '/movie_info' do
+    if params[:movie_title] == ''
+        redirect to ('/')
+    end
+    result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{params[:movie_title]}")
+    @movie = result.parsed_response["Title"].to_s 
+    @genre= result.parsed_response["Genre"].to_s
+    @plot = result.parsed_response["Plot"].to_s
+    @awards= result.parsed_response["Awards"].to_s
+    @released = result.parsed_response["Released"].to_s
+    @poster = result.parsed_response["Poster"]
+    erb (:movie)
+end
+
