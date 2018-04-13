ab5967017797eebf58b858faf9d70db1dd3b997f
kasia-misirli <kasia.misirli@gmail.com>
2018-04-13 15:46:15 +1000

    movies fived functionality

--- Kasia/wk05/1-mon/main.rb
+++ Kasia/wk05/1-mon/main.rb
@@ -3,6 +3,7 @@ require 'sinatra/reloader'
 require 'httparty'
 require 'pry'
 
+
 get '/' do
     erb(:index)
 end
@@ -19,38 +20,15 @@ get '/options' do
     if params[:movie_title] == ''
         redirect to ('/')
     end
-    # @multiple["Search"].each do |movie| 
-    # @mov  =   movie["Title"] 
-    # @url = "http://omdbapi.com/?apikey=2f6435d9&t=#{@mov}"  
-    # end
-
     @multiple = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&s=#{params[:movie_title]}")
-    result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{@mov}")
-    @mov = result.parsed_response["Title"].to_s 
-    @genre= result.parsed_response["Genre"].to_s
-    @plot = result.parsed_response["Plot"].to_s
-    @awards= result.parsed_response["Awards"].to_s
-    @released = result.parsed_response["Released"].to_s
-    @poster = result.parsed_response["Poster"]
     erb(:options)
 end
 
-get 'output' do
-
-
-
+get '/movie_info' do
+    result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{params["movie_name"]}")
+    @mov = result.parsed_response["Title"].to_s 
+    @year = result.parsed_response["Year"].to_s
+    @poster = result.parsed_response["Poster"]
+    erb (:movie)
 end
-
-
-# get '/movie_info' do
-   
-#     result = HTTParty.get("http://omdbapi.com/?apikey=2f6435d9&t=#{@mov}")
-#     @mov = result.parsed_response["Title"].to_s 
-#     @genre= result.parsed_response["Genre"].to_s
-#     @plot = result.parsed_response["Plot"].to_s
-#     @awards= result.parsed_response["Awards"].to_s
-#     @released = result.parsed_response["Released"].to_s
-#     @poster = result.parsed_response["Poster"]
-#     erb (:movie)
-# end
 
