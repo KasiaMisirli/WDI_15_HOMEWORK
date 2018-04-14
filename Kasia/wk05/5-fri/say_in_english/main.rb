f45ec8eecc878b4537d6d2b5b69c3f2d2015af2a
kasia-misirli <kasia.misirli@gmail.com>
2018-04-14 17:21:00 +1000

    removed useless code

--- Kasia/wk05/5-fri/say_in_english/main.rb
+++ Kasia/wk05/5-fri/say_in_english/main.rb
@@ -7,23 +7,20 @@ hash = {0=>"zero",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"
         20=>"twenty",30=>"thirty",40=>"forty",50=>"fifty",60=>"sixty",70=>"seventy",80=>"eighty",90=>"ninety"}
 
 print "What number would you like spelled out?:  "
-number = gets.chomp.to_i
 
+number = gets.chomp.to_i
 if number.digits.count == 1
     print hash[number]
 elsif number == 100
     print "hundred"
 elsif  number.to_s.split('').first.to_i == 1
-    tens = {10=>"ten",11=>"eleven",12=>"twelve",13=>"thirteen",14=>"fourteen",15=>"fifteen",16=>"sixteen",
-    17=>"seventeen", 18=>"eighteen",19=>"nineteen"}
-     print tens[number]
+    print hash[number]
 elsif number == 0
     print "zero"
 elsif number.to_s.split('').last.to_i == 0
     print hash[number]
 else arr = number.to_s.split('')
-    tw = {20=>"twenty",30=>"thirty",40=>"forty",50=>"fifty",60=>"sixty",70=>"seventy",80=>"eighty",90=>"ninety",1=>"one",2=>"two",3=>"three",4=>"four",5=>"five",6=>"six",7=>"seven",8=>"eight",9=>"nine"}
-    print tw[((arr.first)+"0").to_i] +"-"+ tw[arr.last.to_i]
+    print hash[((arr.first)+"0").to_i] +"-"+ hash[arr.last.to_i]
 end
 
 
